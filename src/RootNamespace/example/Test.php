6f6066216f1696db7a050906defe418138409aaf
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-08-06 10:27:34 +0100

    update test.php

--- src/RootNamespace/example/Test.php
+++ src/RootNamespace/example/Test.php
@@ -5,4 +5,11 @@ namespace RootNamespace\example;
 class Test
 {
     private $name = "";
+
+    public function setUsernameAttribute($value)
+    {
+        $notUsed = 1;
+        $this->attributes['username'] = "123username";
+        str_replace("username", "name", "");
+    }
 }
